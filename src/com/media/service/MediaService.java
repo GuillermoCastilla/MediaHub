5b53f1edc2c65ea2f3a0ff538d2421b0a34182af
Guillermo <guilleishon@gmail.com>
2026-01-19 19:07:44 +0000

    Anadidas clase MediaService y Utilities

--- src/com/media/service/MediaService.java
+++ src/com/media/service/MediaService.java
@@ -1,4 +1,36 @@
 package com.media.service;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class MediaService {
+    /**
+     * Atributos
+     */
+    private List<MediaContent> library = new ArrayList<>();
+
+    /**
+     * Constructores
+     */
+    public MediaService() {}
+
+    /**
+     * Filtros
+     * @param title
+     * @return
+     */
+    public List filterByTitle(String title){
+        return library.stream()
+                .filter(media -> media.getTitle().contains(title))
+                .collect(Collectors.toList());
+    }
+
+    public List getTopContentByDuration(){
+        return library.stream()
+                .sorted(Comparator.comparing(MediaContent::getDuration).reversed())
+                .limit(5)
+                .toList();
+    }
 }
